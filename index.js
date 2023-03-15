ee56e410ae314caf50e47fcd42627f426186f377
Joshua A. Sarmiento <jasarmiento@briahomes.com.ph>
2023-03-15 09:34:54 +0800

    route multiple files

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
+// const jsonServer = require("json-server");
+// const server = jsonServer.create();
+// const router = jsonServer.router("db.json");
+// const middlewares = jsonServer.defaults();
+// const port = process.env.PORT || 8080;
+
+// server.use(middlewares);
+// server.use(router)
+
+// server.listen(port);
+
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const path = require("path");
+const fs = require("fs");
+
+const dataDirectory = path.join(__dirname, "data"); // path to the directory containing the JSON files
+const files = fs.readdirSync(dataDirectory); // get all files in the directory
+
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080;
 
-server.use(middlewares);
-server.use(router)
+// Load all JSON files as resources
+const resources = {};
+files.forEach((file) => {
+  const resource = file.replace(".json", "");
+  resources[resource] = require(path.join(dataDirectory, file));
+});
 
-server.listen(port);
+const router = jsonServer.router(resources);
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
