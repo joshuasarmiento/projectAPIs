00b19590e122b974e949cfe7830e9856a6b9c371
Joshua Sarmiento <supremojoshua22@gmail.com>
2023-07-03 16:28:28 +0800

    update folder structure

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const jsonServer = require("json-server");
+const path = require("path");
+
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router({
+  "/portfolioEmployment": path.join(__dirname, "data/portfolio/employment.json"),
+  "/portfolioM-mySkills": path.join(__dirname, "data/portfolio/mySkills.json"),
+  "/portfolioProject": path.join(__dirname, "data/portfolio/project.json"),
+  "/reactBlogStarter": path.join(__dirname, "data/react-blog-starter/blogs.json"),
+});
+
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080;
 
 server.use(middlewares);
-server.use(router)
+server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
